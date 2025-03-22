a150ba4511253b9f4b9054991bf1d1fb51224dc1
sadeenaloran <sadeen.oran@hotmail.com>
2025-03-22 15:40:10 +0300

    update th new

--- Assignment-JS-3/script.js
+++ Assignment-JS-3/script.js
@@ -6,3 +6,20 @@ function factorial(num){
     return result;
 }
 console.log(factorial(5));
+
+let arr=[1, 2, 3, 4, 5];
+let triple = arr.map((x)=> x*3);
+console.log(triple);
+
+let arr1=[1, 2,2, 3,3, 4, 5];
+let unique = arr1.filter((i, j, uni) => uni.indexof(i) === j );
+console.log(unique);
+
+let array=[
+    {name:"hussam", age:30},
+    {name:"Ali", age:40},
+    {name:"Ahmad", age:22},
+];
+
+let sorting= array.sort((a, b) => a.age - b.age);
+console.log(sorting);
